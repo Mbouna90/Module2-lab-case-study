d45df67e8870432f0cf03eeb6ef732eec383c3ac
Amel90 <ameldany18@gmail.com>
2026-03-26 08:37:24 -0400

    Add python assignment file

---
+++ Mod2_prgmnt_assignmt_loop_Amelie.py
@@ -0,0 +1,60 @@
+# Name: Amelie
+# Assignment: Python Exercises 
+# Description:
+# This program demonstrates:
+# - Choosing a number
+# - Using if/else statements
+# - Using a for loop
+# - Assigning and modifying variables
+
+
+# -----------------------------
+# 4.1 Choose a number between 1 and 10
+# -----------------------------
+# In this section, we assign a number between 1 and 10 to a variable.
+
+number = 7
+print("4.1 The chosen number is:", number)
+
+
+# -----------------------------
+# 4.2 If/Else Statements
+# -----------------------------
+# This section demonstrates how to use if/else conditions.
+
+if number < 5:
+    print("4.2 The number is less than 5")
+elif number == 5:
+    print("4.2 The number is equal to 5")
+else:
+    print("4.2 The number is greater than 5")
+
+
+# -----------------------------
+# 6.1 For Loop
+# -----------------------------
+# This section uses a for loop to print values in a list.
+
+my_list = [1, 2, 3, 4, 5]
+
+print("6.1 Printing list values:")
+for item in my_list:
+    print(item)
+
+
+# -----------------------------
+# 6.2 Assign value 7 to guess_me
+# -----------------------------
+# Assigning value 7 to the variable guess_me.
+
+guess_me = 7
+print("6.2 guess_me is:", guess_me)
+
+
+# -----------------------------
+# 6.3 Assign value 5 to guess_me
+# -----------------------------
+# Changing the value of guess_me to 5.
+
+guess_me = 5
+print("6.3 guess_me is now:", guess_me)
